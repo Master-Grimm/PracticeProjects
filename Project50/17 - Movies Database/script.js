f1a227e377fa5c93da3a7236e8669616206abd42
MasterGrimm <Hazard3394@gmail.com>
2022-04-01 14:58:47 +0300

    Day 17

---
+++ Project50/17 - Movies Database/script.js
@@ -0,0 +1,92 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c926c774b65edd1f188181ea1f457a85&page='
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=c926c774b65edd1f188181ea1f457a85&query="'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const back = document.getElementById('back')
+const next = document.getElementById('next')
+
+let currentPage = 1
+
+next.addEventListener('click', () => {
+  currentPage++
+  console.log(currentPage)
+  buttonsUpdate()
+  getMovies(API_URL + currentPage)
+})
+
+back.addEventListener('click', () => {
+  currentPage--
+  console.log(currentPage)
+  buttonsUpdate()
+  getMovies(API_URL + currentPage)
+})
+
+function buttonsUpdate() {
+  if (currentPage > 1) {
+    back.disabled = false
+  } else {
+    back.disabled = true
+  }
+}
+
+getMovies(API_URL + currentPage)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  showMovies(data.results)
+}
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+    main.appendChild(movieEl)
+  })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
